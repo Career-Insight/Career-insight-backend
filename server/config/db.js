994737c59f60dc4f89961a74fbb10e755075252a
mazenfteha <mazn34903@gmail.com>
2024-03-19 22:31:51 -0700

    move to Atlas and add API collections

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,11 @@ dotenv.config({ path: "./config.env" });
 
 
 mongoose
-    .connect(process.env.MONGO_DB)
+    .connect(process.env.MONGO_DB, {
+        dbName:process.env.DATABASE_NAME
+    })
     .then((conn) => {
     console.log(`Database connected: ${conn.connection.host}`);
-})
+    }).catch((err)=> {
+        console.log(`Error while connecting to Database ${err}`);
+    })
