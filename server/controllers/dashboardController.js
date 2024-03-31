b7ecca70a5bf1cbb198ebcc99f6d3c7551ccfddc
mazenfteha <mazn34903@gmail.com>
2024-03-30 17:21:39 -0700

    Feature : Add Frontend , backend and offering distributions

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -51,10 +51,88 @@ const pldynamic = async (req, res, next) => {
     }
 }
 
+// Anlysis 3 : Get Frontend technologies dynamic
+const frontendDistrubtion = async (req, res, next) => {
+    const { count } = req.params;
+    const topCount = parseInt(count);
+
+    try {
+        if (isNaN(topCount) || topCount <= 0) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Invalid count parameter. Please provide a positive integer.' });
+        }
+
+        if(topCount > 14) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Invalid count parameter. We only have 14 frontend frameworks.' });
+        }
+
+        const result = await Result.aggregate([
+            { $project: { frontend_technologies_distribution: { $objectToArray: '$frontend_technologies_distribution' } } },
+            { $unwind: '$frontend_technologies_distribution' },
+            { $sort: { 'frontend_technologies_distribution.v': -1 } },
+            { $limit: topCount },
+            { $group: { _id: null, top_languages: { $push: '$frontend_technologies_distribution' } } },
+            { $project: { _id: 0, top_languages: 1 } }
+        ]);
+
+        res.status(StatusCodes.OK).json(result[0].top_languages);
+    } catch (error) {
+        next(error);
+    }
+}
+
+// Anlysis 4 : Get Backend technologies dynamic
+const backendDistrubtion = async (req, res, next) => {
+    const { count } = req.params;
+    const topCount = parseInt(count);
+
+    try {
+        if (isNaN(topCount) || topCount <= 0) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Invalid count parameter. Please provide a positive integer.' });
+        }
+
+        if(topCount > 55) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Invalid count parameter. We only have 55 backend technologies.' });
+        }
+
+        const result = await Result.aggregate([
+            { $project: { backend_technologies_distribution: { $objectToArray: '$backend_technologies_distribution' } } },
+            { $unwind: '$backend_technologies_distribution' },
+            { $sort: { 'backend_technologies_distribution.v': -1 } },
+            { $limit: topCount },
+            { $group: { _id: null, top_languages: { $push: '$backend_technologies_distribution' } } },
+            { $project: { _id: 0, top_languages: 1 } }
+        ]);
+
+        res.status(StatusCodes.OK).json(result[0].top_languages);
+    } catch (error) {
+        next(error);
+    }
+}
+
+// Anlysis 4 : Get offerings_distribution
+const offeringsDistribution = async (req, res, next) => {
+    try {
+        const result = await Result.aggregate([
+            { $project: { offerings_distribution: { $objectToArray: '$offerings_distribution' } } },
+            { $unwind: '$offerings_distribution' },
+            { $sort: { 'offerings_distribution.v': -1 } },
+            { $limit: 3 },
+            { $group: { _id: null, top_languages: { $push: '$offerings_distribution' } } },
+            { $project: { _id: 0, top_languages: 1 } }
+        ]);
+        res.status(StatusCodes.OK).json(result[0].top_languages);
+        
+    } catch (error) {
+        next(error)
+    }
+}
 
 
 
 module.exports = { 
     plTopTen,
-    pldynamic
+    pldynamic,
+    frontendDistrubtion,
+    backendDistrubtion,
+    offeringsDistribution
 }
--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { plTopTen, pldynamic } = require('../controllers/dashboardController')
+const { 
+        plTopTen,
+        pldynamic,
+        frontendDistrubtion,
+        backendDistrubtion,
+        offeringsDistribution
+    }
+= require('../controllers/dashboardController')
 
 router.get('/general/programming-languages/top-ten',plTopTen )
 router.get('/general/programming-languages/:count',pldynamic )
 
-router.get('/general/frontend-technologies', )
-router.get('/general/backend-technologies', )
-router.get('/general/offering-distributions', )
+router.get('/general/frontend-technologies/:count',frontendDistrubtion )
+router.get('/general/backend-technologies/:count',backendDistrubtion )
+router.get('/general/offering-distributions', offeringsDistribution)
 
 
 
