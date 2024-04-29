f0caf440243257281101013a7e665a97bf25c4eb
mazenfteha <mazn34903@gmail.com>
2024-04-29 13:51:54 -0700

    Update API docs

--- server/controllers/companiesController.js
+++ server/controllers/companiesController.js
@@ -3,6 +3,10 @@ const Company = require('../models/Company');
 //get company data by it's name 
 const getCompanyByName = async (req, res, next) => {
     const company_name = req.query.company_name;
+    const companyExists = await Company.exists({ company_name });
+        if (!companyExists) {
+            return res.send('Company not found');
+        }
     await Company.findOne({ company_name })
         .then(company => {
             res.json(company);
