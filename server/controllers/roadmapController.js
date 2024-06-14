a28ea3a29af96411292ecb8a5cb683fe1d0631a9
mazenfteha <mazn34903@gmail.com>
2024-06-14 14:42:01 -0700

    feat : Roadmap for user add another two APIs

--- server/controllers/roadmapController.js
+++ server/controllers/roadmapController.js
@@ -43,10 +43,35 @@ const getAllUserRoadmaps = async (req , res , next) => {
 }
 
 // get a specific user roadmap by ID
-// update a specific user roadmap by ID
+const getSpecificRoadmapbyId = async (req, res, next) => {
+    try {
+        const roadmap = await userRoadMap.findOne({ user_id: req.params.userId, _id: req.params.roadmapId });
+        if (!roadmap) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: 'Roadmap not found' });
+        }
+        res.status(StatusCodes.OK).json(roadmap);
+    } catch (error) {
+        next(error)
+    }
+}
+// update a specific user roadmap by ID (wait for update logic)
+//delete Roadmap by id
+const deleteRoadmapById = async (req, res, next) => {
+    try {
+        const userRoadmap = await userRoadMap.findOneAndDelete({ user_id: req.params.userId, _id: req.params.roadmapId });
+        if (!userRoadmap) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: 'Roadmap not found' });
+        }
+        res.status(StatusCodes.OK).json({ message: 'Roadmap deleted successfully' });
+    } catch (error) {
+        next(error)
+    }
+}
 
 
 module.exports = {
     selectAndSaveUserRoadmap,
-    getAllUserRoadmaps
+    getAllUserRoadmaps,
+    getSpecificRoadmapbyId,
+    deleteRoadmapById
 }
--- server/routes/roadmapRoutes.js
+++ server/routes/roadmapRoutes.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { selectAndSaveUserRoadmap, getAllUserRoadmaps } = require('../controllers/roadmapController');
+const 
+    { 
+        selectAndSaveUserRoadmap,
+        getAllUserRoadmaps,
+        getSpecificRoadmapbyId,
+        deleteRoadmapById
+    } 
+    = require('../controllers/roadmapController');
 
 router.post('/select-and-save', selectAndSaveUserRoadmap)
 router.get('/user/:userId', getAllUserRoadmaps)
+router.get('/user/:userId/roadmap/:roadmapId', getSpecificRoadmapbyId)
+router.delete('/user/:userId/roadmap/:roadmapId', deleteRoadmapById)
+
+
 
 
 module.exports = router 
