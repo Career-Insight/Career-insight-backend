55e6d7c0593568407de4de38701dd4e39e172348
mazenfteha <mazn34903@gmail.com>
2024-06-17 23:23:44 -0700

    make model for market demand and update CI chat

--- server/controllers/ci-chat-controller.js
+++ server/controllers/ci-chat-controller.js
@@ -10,12 +10,13 @@ const generateRoadmap = async (req, res, next) => {
         const { prompt } = req.body;
 
         const response = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo-16k-0613',
-            prompt: prompt,
-            max_tokens: 150,
+            model: 'ft:gpt-3.5-turbo-0125:personal::9ZHgO91t:ckpt-step-26',
+            messages: [{ role: 'system', content: `content: As an instructor, help me to generate a roadmap or learning path suitable for the market demand in the IT field for${prompt}` }],
+            max_tokens: 1000,
         });
 
-        res.json({ response: response.data.choices[0].text });
+        res.json({ response: response.choices[0].message.content});
+        console.log(response);
     } catch (error) {
         console.error('Error communicating with OpenAI API:', error);
         next(error)
